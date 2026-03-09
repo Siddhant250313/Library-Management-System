dbf29c0a89b2a346bfe0507bcea2ecdafb8605c6
Siddhant250313 <siddhantpardeshi13@gmail.com>
2026-03-09 09:56:02 +0530

    Mask database password in DBConn.java
    Replaced the database password with a placeholder.

--- src/Connection1/DBConn.java
+++ src/Connection1/DBConn.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBConn {
     private static final String URL = "jdbc:mysql://localhost:3306/library";
     private static final String USER = "root";
-    private static final String PASSWORD = "siddhant_2503";
+    private static final String PASSWORD = "***********";
 
     public static Connection getConnection() {
         try {
